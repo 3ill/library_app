0b04f31b52401d106eb964f3f57e470e295c4fb4
3illBaby <thrillanon@outlook.com>
2025-01-10 23:53:02 +0100

    chore(page.tsx): impl static home page

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,12 +1,17 @@
 import BookList from "@/components/book-list";
 import BookOverview from "@/components/book-overview";
 import { Button } from "@/components/ui/button";
+import { sampleBooks } from "@/constants";
 
 const Home = () => {
   return (
     <>
-      <BookOverview />
-      <BookList />
+      <BookOverview {...sampleBooks[0]} />
+      <BookList
+        title="Latest Books"
+        books={sampleBooks}
+        containerClassName="mt-28"
+      />
     </>
   );
 };
