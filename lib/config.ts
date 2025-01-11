e227db173e114e85bd00beabda3dbfe8a1da5088
3illBaby <thrillanon@outlook.com>
2025-01-11 20:44:47 +0100

    chore(config.ts): added env config object

---
+++ lib/config.ts
@@ -0,0 +1,9 @@
+const config = {
+  env: {
+    urlEndpount: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT,
+    publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_URL_PUBLIC_KEY,
+    privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
+  },
+};
+
+export default config;
