506bc3f24a22e1b8cc95065459bf74dda06ea9bd
3illBaby <thrillanon@outlook.com>
2025-01-13 21:50:19 +0100

    chore(config.ts): extended config with db keys

--- lib/config.ts
+++ lib/config.ts
@@ -1,8 +1,12 @@
 const config = {
   env: {
-    urlEndpount: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT,
-    publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_URL_PUBLIC_KEY,
-    privateKey: process.env.IMAGE_KIT_PRIVATE_KEY,
+    apiEndpoint: process.env.NEXT_PUBLIC_API_ENDPOINT!,
+    imageKit: {
+      urlEndpoint: process.env.NEXT_PUBLIC_IMAGEKIT_URL_ENDPOINT!,
+      publicKey: process.env.NEXT_PUBLIC_IMAGEKIT_PUBLIC_KEY!,
+      privateKey: process.env.PRIVATE_KEY!,
+    },
+    databaseUrl: process.env.DATABASE_URL!,
   },
 };
 
