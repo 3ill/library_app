6ba36d8ad8d9d18019d3e7c6afa570bd4f8e27b1
3illBaby <thrillanon@outlook.com>
2025-01-14 20:30:22 +0100

    chore(utils.ts): impl func to get initials

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,10 @@
-import { clsx, type ClassValue } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { clsx, type ClassValue } from "clsx";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const getInitials = (name: string): string => {
+  return name.replace(/\s/g, "").slice(0, 2).toUpperCase();
+};
