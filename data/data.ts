5f1bf53ea5d32ee58a44c44dade60be06aa64059
3illBaby <thrillanon@outlook.com>
2025-01-14 20:32:07 +0100

    chore(data.ts): added sign in success data

---
+++ data/data.ts
@@ -0,0 +1,6 @@
+export enum AuthenticationData {
+  SIGN_UP_FAILED = "sign up error",
+  SIGN_UP_SUCCESS = "sign up success",
+  SIGN_IN_FAILED = "sign in error",
+  SIGN_IN_SUCCESS = "sign in success",
+}
