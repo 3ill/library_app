83cbe88d1d698bbc9c553b7428845b000d397ed5
3illBaby <thrillanon@outlook.com>
2025-01-16 21:25:05 +0100

    refactor(workflow.ts): workflow.ts
    converted Qstash lib to class

--- lib/workflow.ts
+++ lib/workflow.ts
@@ -1,7 +1,40 @@
 import { Client as WorkFlowClient } from "@upstash/workflow";
+import { Client as QStashClient, resend } from "@upstash/qstash";
 import config from "./config";
 
-export const workFlowClient = new WorkFlowClient({
-  baseUrl: config.env.upstash.qstashUrl,
-  token: config.env.upstash.qstashToken,
-});
+interface ISendEmail {
+  email: string;
+  subject: string;
+  message: string;
+}
+
+export class QStashRepository {
+  getWorkFlowClient() {
+    return new WorkFlowClient({
+      baseUrl: config.env.upstash.qstashUrl,
+      token: config.env.upstash.qstashToken,
+    });
+  }
+
+  getQstashClient() {
+    return new QStashClient({
+      token: config.env.upstash.qstashToken,
+    });
+  }
+
+  sendEmail = async (args: ISendEmail) => {
+    const { email, subject, message } = args;
+    await this.getQstashClient().publishJSON({
+      api: {
+        name: "email",
+        provider: resend({ token: config.env.resendToken }),
+      },
+      body: {
+        from: "3illBaby <updates.dsacorp.xyz>",
+        to: [email],
+        subject,
+        html: message,
+      },
+    });
+  };
+}
