493141087c6db81c497bcf6be121e49507631da0
3illBaby <thrillanon@outlook.com>
2025-01-16 21:21:08 +0100

    feat(layout.tsx): update activity timestamp
    update last actvity date after the root component is rendered

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,10 @@
 import { auth } from "@/auth";
 import Header from "@/components/header";
+import { db } from "@/database/drizzle";
+import { users } from "@/database/schema";
+import { eq } from "drizzle-orm";
 import { redirect } from "next/navigation";
+import { after } from "next/server";
 import { ReactNode } from "react";
 
 interface ILayoutProps {
@@ -10,6 +14,25 @@ interface ILayoutProps {
 const Layout = async ({ children }: ILayoutProps) => {
   const session = await auth();
   if (!session) return redirect("/sign-in");
+
+  after(async () => {
+    if (!session?.user?.id) return;
+
+    const user = await db
+      .select()
+      .from(users)
+      .where(eq(users.id, session?.user?.id))
+      .limit(1);
+
+    if (user[0].lastActivityDate === new Date().toISOString().slice(0, 10))
+      return;
+
+    await db
+      .update(users)
+      .set({ lastActivityDate: new Date().toISOString().slice(0, 10) })
+      .where(eq(users.id, session?.user?.id));
+  });
+
   return (
     <main className="root-container">
       <div className="mx-auto max-w-7xl">
